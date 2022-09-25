92cf81fe066d684270fa33dfc98a91095497b211
Lautaro Abou-Eid <70718664+aboueidlautaro@users.noreply.github.com>
2022-09-25 17:44:58 -0300

    Create LayoutRequireAuth.jsx

---
+++ src/assets/modules/LayoutRequireAuth.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+
+const LayoutRequireAuth = (authState, { children }) => {
+  if (!authState.status) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+export default LayoutRequireAuth;
